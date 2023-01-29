b4929b508bc1769781bc4c607d101e0aa8cae3df
Chaitanya <cgandham@opentext.com>
2023-01-29 12:30:14 +0530

    first commit

--- src/app/micro-frontends/bookings-host.component.ts
+++ src/app/micro-frontends/bookings-host.component.ts
@@ -8,18 +8,5 @@ import { Component, OnInit } from '@angular/core';
 
 export class BookingsHostComponent implements OnInit{
     ngOnInit() {
-        // this.load('http://localhost:4201/main_child.js');
     }
- 
-    // load(url: string): Promise<void> {
-    //     return new Promise((resolve, reject) => {
-    //         const script = document.createElement('script');
-    //         script.src = url;
-    //         script.onload = () => resolve();
-    //         script.onerror = ()  => reject({
-    //             error: `Bundle ${url} could not be loaded`,
-    //         });
-    //         document.body.appendChild(script);
-    //     })
-    // }
 }
